ecaaf0191b4043214db646488ef4709c065ea839
Gideonthegre8t <timilehingabbana502@gmail.com>
2023-03-14 10:54:05 -0700

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,3 @@
-#include "main.h"
-
-int main(void)
-{
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
-}
+#!/bin/bash
+gcc -Wall -Wextra -Werror -pedantic -c *.c
+ar rcs liball.a *.o
